85be753ec20ffb8c84372d4fb83cb332c6e56d14
Andrej <zarke998@gmail.com>
2019-06-21 02:42:48 +0200

    Added navigation and headerIntro (partially finished

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    duration: 500
+  });
+autoplay();
+function autoplay() {
+    $('.carousel.carousel-slider').carousel('next');
+    setTimeout(autoplay, 8500);
+}
