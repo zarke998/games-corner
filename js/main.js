839cb162914ca1f7bd6c7548bb22cef8c08c28b0
Andrej <zarke998@gmail.com>
2019-06-23 23:44:56 +0200

    News page finished

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ $('.carousel.carousel-slider').carousel({
     indicators: true,
     duration: 500
   });
+  
 autoplay();
 function autoplay() {
     $('.carousel.carousel-slider').carousel('next');
     setTimeout(autoplay, 8500);
 }
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
