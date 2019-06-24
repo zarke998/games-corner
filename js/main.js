3790e2f5cee6d48f04fcb9d5c123678a7b8018ad
Andrej <zarke998@gmail.com>
2019-06-25 00:30:29 +0200

    Game list page finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Init carousel(bootstrap)
 $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true,
@@ -10,6 +11,26 @@ function autoplay() {
     setTimeout(autoplay, 8500);
 }
 
+// Init select(materialize)
 $(document).ready(function(){
   $('select').formSelect();
+});
+
+// Init noui-slider
+var nonLinearStepSlider = document.getElementById('noui-slider');
+
+noUiSlider.create(nonLinearStepSlider, {
+    start: [20, 99],
+    connect: true,
+    range: {
+        'min': [0],
+        '10%': [10, 5],
+        'max': [150]
+    }
+});
+
+var nonLinearStepSliderValueElement = document.getElementById('noui-slider-values');
+
+nonLinearStepSlider.noUiSlider.on('update', function (values) {
+    nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
 });
