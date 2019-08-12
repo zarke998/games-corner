9c4dfdcbe0daa9c311568039c23f028be1405611
Andrej <zarke998@gmail.com>
2019-08-12 13:33:19 +0200

    Added RGB

--- js/main.js
+++ js/main.js
@@ -20,6 +20,22 @@ $(document).ready(function(){
     $('.materialboxed').materialbox();
   });
 
+
+// RGB Switch
+$(document).ready(function(){
+  $("#rgbSwitch input[type='checkbox'").change(function(){
+    if(this.checked){
+      $(".seperator-wrapper").css("display", "block");
+      $(".headerNav").css("border-bottom-style", "hidden");
+      $("#footer").css("border-top-style", "hidden");
+    }
+    else{
+      $(".seperator-wrapper").css("display", "none");
+      $(".headerNav").css("border-bottom-style", "solid");
+      $("#footer").css("border-top-style", "solid");
+    }
+    });
+});
      
 // Init noui-slider
 var nonLinearStepSlider = document.getElementById('noui-slider');
@@ -39,3 +55,6 @@ var nonLinearStepSliderValueElement = document.getElementById('noui-slider-value
 nonLinearStepSlider.noUiSlider.on('update', function (values) {
     nonLinearStepSliderValueElement.innerHTML = values.join(' - ');
 });
+
+
+
